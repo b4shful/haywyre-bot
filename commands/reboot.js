93c5743d2efb7f6d83939c13318f2398ee3e5ad8
Joel Murphy <joelmurphy111@btinternet.com>
2018-02-24 00:30:47 +0000

    changed shutdown message to reboot in reboot.js

--- commands/reboot.js
+++ commands/reboot.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args, level) => {// eslint-disable-line no-unused-vars
-  await message.reply("Bot is shutting down.");
+  await message.reply("Bot is rebooting.");
   client.commands.forEach( async cmd => {
     await client.unloadCommand(cmd);
   });
