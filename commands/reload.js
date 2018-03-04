4d77705309e1f8d29351caeab250ee4419c4f476
Joel Murphy <joelmurphy111@btinternet.com>
2018-03-04 13:35:03 +0000

    remove test agh

--- commands/reload.js
+++ commands/reload.js
@@ -9,7 +9,7 @@ exports.run = async (client, message, args, level) => {// eslint-disable-line no
 
   message.reply(`The command \`${args[0]}\` has been reloaded`);
 };
-//test6
+
 exports.conf = {
   enabled: true,
   guildOnly: false,
