497eab2f1ab56de3e909b98378befc8dc29fdedb
Joel Murphy <joelmurphy111@btinternet.com>
2018-04-18 23:33:27 +0100

    provided a slight fix for the delete command

--- commands/clear.js
+++ commands/clear.js
@@ -1,20 +1,28 @@
 //Clears a specific number of messages from a text channel.
+const Logger = require('../util/Logger');
 
 exports.run = async (client, message, args) => { // eslint-disable-line no-unused-vars
 
-//adds 1 to the counted messages, to account for the command message itself
-  const deleteCount = parseInt((args[0], 10) + 1);
+  const deleteCount = parseInt(args[0], 10);
     
-  if (!deleteCount || deleteCount < 1 || deleteCount > 100)
-    return message.reply("Please provide a number between 1 and 100 for the number of messages to delete");
+  if (!deleteCount || deleteCount < 1 || deleteCount > 99)
+    return message.reply("Please provide a number between 1 and 99 for the number of messages to delete");
 
-  const fetched = await message.channel.fetchMessages({limit: deleteCount});
+  //adds 1 to the counted messages, to account for the command message itself
+  const fetched = await message.channel.fetchMessages({limit: (deleteCount + 1)});
   message.channel.bulkDelete(fetched)
     .catch(error => {
-      console.log(error);
-      message.channel.send("An error occured while running this command.");
+      Logger.error(error);
+      message.channel.send(`Unable to bulk delete messages: \`${error}\``);
     });
-  message.reply(`Cleared ${fetched.size} messages.`);
+  message.reply(`Cleared ${fetched.size - 1} messages.`)
+    .then(msg => {
+      msg.delete(1000);
+    })
+    .catch(error => {
+      Logger.error(error);
+      message.channel.send('Unable to delete temporary message.');
+    } );
 };
     
 exports.conf = {
